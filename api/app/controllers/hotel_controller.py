2b04e5260c939f7ce1c4da8d90c6803c1e55c839
viktor <phuong.nguyen@tui.co.uk>
2023-07-17 17:48:40 +0700

    add api hotel

--- api/app/controllers/hotel_controller.py
+++ api/app/controllers/hotel_controller.py
@@ -1,7 +1,8 @@
 import string
+from app.db.mongo.documents.amenity_provider_mapping import AmenityProviderMapping
 from flask import Blueprint, jsonify
 from app.db.mongo.documents.property import Property
-from flask import request
+from flask import request, make_response
 
 hotel_controller = Blueprint('hotel_controller', __name__)
 
@@ -79,18 +80,55 @@ def get_hotel_ids(region_id: string):
     amenities = json_data.get('amenities')
     places = json_data.get('places')
 
+    match_multiple_condition = [
+        {"$in": ["$region_ids", region_id]}
+    ]
 
-
-    pipeline = [
-        {"$match": {"region_ids": {"$in": [region_id]}}},
-        {"$unwind": "$amenity_ids"},
-        {
-            "$group": {
+    if amenities is not None and len(amenities) != 0:
+        provider_amenities_pipeline = [
+            {"$match": {"$expr": {"$and": [{"$in": ["$source_code", amenities]}, {
+                "$eq": ["$source_provider", "bamboo"]}]}}},
+            {"$group": {
                 "_id": None,
-                "stars": {"$addToSet": "$star"},
-                "amenities": {"$addToSet": "$amenity_ids"}
-            }
-        },
+                "amenity_provider_ids": {"$addToSet": "$destination_code"}
+            }},
+            {"$project": {
+                "amenity_provider_ids": "$amenity_provider_ids"
+            }}
+        ]
+
+        provider_amenities = list(
+            AmenityProviderMapping.objects.aggregate(provider_amenities_pipeline))
+        if provider_amenities is None:
+            return make_response("failure", 400)
+
+        match_multiple_condition.append(
+            {"$in": ["$amenity_ids", provider_amenities]})
+
+    if stars is not None and len(stars) != 0:
+        match_multiple_condition.append({"$in": ["$star", stars]})
+
+    if places is not None and len(places) != 0:
+        match_multiple_condition.append(
+            {"places": {"$elemMatch": {"id": {"$in": places}, "code": "multi_city_vicinity"}}})
+
+    match_multiple_condition.append({
+        "$project": {
+            "hotel_ids": "$goquo_id"
+        }
+    })
+
+    properties = Property.objects.aggregate(match_multiple_condition)
+    return jsonify(list(properties))
+
+
+@hotel_controller.route("/api/get-hotels", methods=["POST"])
+# @abac_authorize()
+def get_hotel():
+    json_data = request.get_json()  # Lấy dữ liệu từ JSON body
+    hotel_ids = json_data.get('hotel_ids')
+    pipeline = [
+        {"$match": {"goquo_id": {"$in": hotel_ids}}},
         {
             "$lookup": {
                 "from": "amenity_provider_mapping",
@@ -116,48 +154,13 @@ def get_hotel_ids(region_id: string):
                     }}],
                 "as": "bamboo_amenities_mapping"}
         },
+        {"$set": {
+            "amenities": "$bamboo_amenities_mapping"
+        }},
         {
-            "$project": {
-                "stars": "$stars",
-                "amenities": {
-                    "$map": {
-                        "input": "$bamboo_amenities_mapping",
-                        "as": "mapping",
-                        "in": {
-                            "$let": {
-                                "vars": {
-                                    "first_amenity": {"$arrayElemAt": ["$$mapping.bamboo_amenities", 0]}
-                                },
-                                "in":{
-                                    "value": {"$arrayElemAt": ["$$first_amenity.value.value", 0]},
-                                    "code":"$$first_amenity.code"
-                                }
-                            }}
-                    }}
+            "$unset": {
+                "amenity_ids": ""
             }
         }
     ]
-
-    properties = Property.objects.aggregate(pipeline)
-    return jsonify(list(properties))
-
-
-@hotel_controller.route("/api/get-hotels", methods=["POST"])
-# @abac_authorize()
-def get_hotel():
-    json_data = request.get_json()  # Lấy dữ liệu từ JSON body
-    region_ids = json_data.get('region_ids')
-    pipeline = [
-        {"$match": {"region_ids": {"$in": region_ids}}},
-        {"$unwind": "$amenity_ids"},
-        {
-            "$group": {
-                "_id": None,
-                "stars": {"$addToSet": "$star"},
-                "amenities": {"$addToSet": "$amenity_ids"}
-            }
-        }
-    ]
-
-    result = Property.objects.aggregate(pipeline)
-    return jsonify(list(result))
+    return jsonify(list(Property.objects.aggregate(pipeline)))
